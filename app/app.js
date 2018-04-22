8640b201b8c5754dc6285dcf2cf58bf9e062c6e1
wuguoyao <wuguoyao@baidu.com>
2018-04-22 15:12:24 +0800

    引入redux

--- app/app.js
+++ app/app.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route} from 'react-router';
+import {Route} from 'react-router';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'react-router-redux';
 import {createBrowserHistory} from 'history';
 
 import Shell from './containers/Shell';
 import {route as HomePageRoute} from './containers/HomePage';
 import {route as NotFoundPageRoute} from './containers/NotFoundPage';
 
+import configureStore from './configureStore';
+
+const initialState = {};
 const history = createBrowserHistory();
+const store = configureStore(initialState, history);
+
 const routes = [].concat(
     HomePageRoute,
     NotFoundPageRoute
 );
 
 ReactDOM.render(
-    <Router history={history}>
-        <Route render={props => <Shell {...props} routes={routes} />} />
-    </Router>,
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Route render={props => <Shell {...props} routes={routes} />} />
+        </ConnectedRouter>
+    </Provider>,
     document.getElementById('app')
 );
