b8927b36357d82c9730984e82c31cb34efe04fa6
wuguoyao <wuguoyao@baidu.com>
2018-04-22 11:32:44 +0800

    添加less支持

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -7,8 +7,10 @@ import React from 'react';
 import {Switch, Redirect} from 'react-router';
 import {Link} from 'react-router-dom';
 
+import './App.less';
+
 export default props => (
-    <div>
+    <div className="app">
         <h1>APP</h1>
         <Link to="/home">Home</Link>
         <Link to="/404">404 Page</Link>
